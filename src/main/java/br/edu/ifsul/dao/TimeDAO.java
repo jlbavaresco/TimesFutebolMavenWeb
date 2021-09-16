c59ddfff1ef81dbd0e7ce484040742fb44148e60
Jorge Luis Boeira Bavaresco <jlbavaresco@gmail.com>
2021-09-16 09:24:32 -0300

    ajustes em relatórios

--- src/main/java/br/edu/ifsul/dao/TimeDAO.java
+++ src/main/java/br/edu/ifsul/dao/TimeDAO.java
@@ -38,7 +38,7 @@ public class TimeDAO<TIPO>  extends DAOGenerico<Time> implements Serializable {
     }    
     
     public List<Time> getListaObjetosCompleta(){
-        String jpql = "select distinct t from Time t join fetch t.jogadores order by t.id";
+        String jpql = "select distinct t from Time t left join fetch t.jogadores order by t.id";
         return em.createQuery(jpql).getResultList();
     }
 
